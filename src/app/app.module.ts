9f13004ad4fc5449e50508fb30b325c9fac6c5bf
Steven Nguyen <snguyen@oakland.edu>
2018-02-18 01:04:05 -0500

    minor UI change to project list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import {Ng2PageScrollModule} from 'ng2-page-scroll';
+import { Ng2PageScrollModule } from 'ng2-page-scroll';
 import { NgxCarouselModule } from 'ngx-carousel';
 import 'hammerjs';
 
